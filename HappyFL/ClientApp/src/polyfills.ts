ff698362dfd35758ffaa101f5c9b73b026e2d8f7
Yas <yasu.gemini@gmail.com>
2020-07-26 14:25:29 +1200

    migrate from Angular 8 to Angular 10

--- HappyFL/ClientApp/src/polyfills.ts
+++ HappyFL/ClientApp/src/polyfills.ts
@@ -73,8 +73,8 @@ import 'core-js/es7/reflect';
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
-
-
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+import '@angular/localize/init';
